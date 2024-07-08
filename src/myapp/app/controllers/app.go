1d5076bd122fc83ecab49476ed3dd22d2e7f6c95
dong.kieuxuan <dong-kieuxuan@gmo.jp>
2024-07-09 02:50:15 +0900

    add code to check lint

--- src/myapp/app/controllers/app.go
+++ src/myapp/app/controllers/app.go
@@ -10,5 +10,14 @@ type App struct {
 
 func (c App) Index() revel.Result {
 	indexVariable := "Index Variable"
+	isValueValid(indexVariable)
 	return c.Render(indexVariable)
+}
+
+func (c App) isValueValid(value string) boolean {
+	if value == "valid" {
+		return true
+	}
+
+	return false
 }
